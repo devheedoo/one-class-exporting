317d856e7fa66b3e0ac0af74a6bb5ab448d9b6d4
Heedo Kim(Choco) on Mac Mini <devheedoo@gmail.com>
2020-01-30 10:50:49 +0900

    Implement OneClassExporting
    IMPORTANT:
    - In package.json, "main": "dist/index.js", "types": "dist/index.d.ts"
    - In tsconfig.json, "declaration": true

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+class OneClassExporting {
+  _property1: string = '';
+  _property2: number = 0;
+
+  constructor(property1?: string, property2?: number) {
+    if (property1) this._property1 = property1;
+    if (property2) this._property2 = property2;
+  }
+
+  setProperty1(property1: string): void {
+    this._property1 = property1;
+  }
+
+  setProperty2(property2: number): void {
+    this._property2 = property2;
+  }
+
+  toString(): string {
+    return `${this._property1} ${this._property2}`
+  }
+}
+
+export default OneClassExporting;
